c1ac238939fad280e503be2238f16d40f9f8246c
chiragbarua1726-art <chirag.barua1726@gmail.com>
2026-06-24 01:45:56 +0530

    Time: 12 ms (43.55%), Space: 11.8 MB (53.29%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,10 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head==NULL)
-        return false;
-        if(head->next==NULL)
-        return false;
+        
 
         ListNode* slow = head;
         ListNode* fast = head;
