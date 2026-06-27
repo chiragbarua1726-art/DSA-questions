87b0dafac7b0735e3844c528e7dd06fa467eead9
chiragbarua1726-art <chirag.barua1726@gmail.com>
2026-06-27 18:05:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.2%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long res = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+
+            if (res > INT_MAX / 10 || res < INT_MIN / 10)
+                return 0;
+
+            res = res * 10 + digit;
+            x = x / 10;
+        }
+
+        return res;
+    }
+};
