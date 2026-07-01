8a1fe7d0a353cbe79a21677c5bfcda2321090e99
chiragbarua1726-art <chirag.barua1726@gmail.com>
2026-07-01 17:47:41 +0530

    Time: 9 ms (12.43%), Space: 12.3 MB (11.01%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool fun (unordered_map<char,int> have, unordered_map<char,int> need)
+    {
+        for(auto i : need)
+        {
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+
+            if(fneed > fhave)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> have; 
+        unordered_map<char,int> need;
+        
+        for(int i = 0 ; i<ransomNote.size() ; i++)
+        {
+            need[ransomNote[i]]++;
+        }
+        for(int i = 0 ; i<magazine.size() ; i++)
+        {
+            have[magazine[i]]++;
+        }
+        return fun(have,need);
+    }
+};
