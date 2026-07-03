89a4b6df5be8969b2a68a08e9a984e04e4d30319
chiragbarua1726-art <chirag.barua1726@gmail.com>
2026-07-03 18:53:05 +0530

    Time: 0 ms (100%), Space: 11.3 MB (19.79%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -0,0 +1,87 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    void reverse(ListNode* head, int times)
+    {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
+        while(times--)
+        {
+            ListNode* nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
+        }
+        return;
+    }
+
+    ListNode* swapPairs(ListNode* head) {
+
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        int size = 2;
+
+        ListNode* left = head;
+        ListNode* prevleft = NULL;
+        ListNode* res = NULL;
+        ListNode* right;
+
+        while(true)
+        {
+            right = left;
+
+            
+            for(int i = 0; i < size - 1; i++)
+            {
+                if(right == NULL)
+                    break;
+
+                right = right->next;
+            }
+
+            if(right)
+            {
+                ListNode* nextleft = right->next;
+
+                reverse(left, size);
+
+                if(prevleft)
+                    prevleft->next = right;
+                else
+                    res = right;
+
+                left->next = nextleft;
+
+                prevleft = left;
+                left = nextleft;
+
+                if(left == NULL)
+                    break;
+            }
+            else
+            {
+                if(prevleft)
+                    prevleft->next = left;
+                else
+                    res = left;
+
+                break;
+            }
+        }
+
+        return res;
+    }
+};
